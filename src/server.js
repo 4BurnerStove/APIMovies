f774d74977513d39112e421faad5382c25f065a0
Andre Morryson <amorrysson77@gmail.com>
2023-05-18 17:28:37 -0300

    Create a AppError, I got the client and server errors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
+require("express-async-errors")
+
 const express = require('express')
+const AppError = require('./utils/AppError')
 const routes = require('./Routes/index')
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use((error, req, res, next) => {
+  if(error instanceof AppError){
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+
+})
 
 const PORT = 4444
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
