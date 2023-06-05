ece980906cd45fe096e485402c8a0ccc486be533
Andre Morryson <amorrysson77@gmail.com>
2023-06-05 14:14:48 -0300

    Scheare for Tags

--- src/controllers/movieNotesController.js
+++ src/controllers/movieNotesController.js
@@ -44,6 +44,38 @@ class movieNotesController{
 
     return res.json()
   }
+
+  async index(req, res){
+    const { user_id, titleMovie, moviesTags } = req.query
+
+    let notes
+
+    if(moviesTags) {
+      const filterTags = moviesTags.split(',').map(tag => tag.trim())
+
+      notes = await knex('moviesTags')
+      .select([
+        "moviesNotes.id",
+        "moviesNotes.titleMovie",
+        "moviesNotes.user_id"
+      ])
+      .where("moviesNotes.user_id", user_id)
+      .whereLike('moviesNotes.titleMovie', `%${titleMovie}%` )
+      .whereIn("name", filterTags)
+      .innerJoin("moviesNotes", "moviesNotes.id", "moviesTags.moviesNotes_id")
+      .orderBy('moviesNotes.titleMovie')
+
+
+    } else {
+    notes = await knex('moviesNotes')
+    .where({ user_id })
+    .whereLike('titleMovie', `%${titleMovie}%`)
+    .orderBy('titleMovie')
+   }
+  res.json(notes)
+  }
+
+
 }
 
 module.exports = movieNotesController
