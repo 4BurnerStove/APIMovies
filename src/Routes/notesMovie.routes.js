2a2fdefb3c8e17c51d6e30cc9ef1750f550c22ec
Andre Morryson <amorrysson77@gmail.com>
2023-09-04 14:21:03 -0300

    Corretions logical error

--- src/Routes/notesMovie.routes.js
+++ src/Routes/notesMovie.routes.js
@@ -7,7 +7,7 @@ const movieNotesRoutes = Router()
 const movieNotesController = new MovieNotesController()
 
 movieNotesRoutes.get('/',  movieNotesController.index)
-movieNotesRoutes.post('/:user_id',  movieNotesController.create)
+movieNotesRoutes.post('/',  movieNotesController.create)
 movieNotesRoutes.get('/:id', movieNotesController.show)
 movieNotesRoutes.delete('/:id', movieNotesController.delete)
 
