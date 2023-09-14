32a7856af84cc131fc18749afca5c992fbdc2014
Andre Morryson <amorrysson77@gmail.com>
2023-09-14 16:24:39 -0300

    Config ambient and add pm2 online server

--- src/server.js
+++ src/server.js
@@ -37,5 +37,5 @@ app.use((error, req, res, next) => {
 
 })
 
-const PORT = process.env.SERVER_PORT || 3000 ;
+const PORT = process.env.SERVER_PORT || 4000 ;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
