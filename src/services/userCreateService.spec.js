2f82ef2c5ad73b51a94f81406165fdce9f323dd5
Andre Morryson <amorrysson77@gmail.com>
2023-09-26 10:48:33 -0300

    Automated tests with jest

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -4,36 +4,39 @@ const AppError = require('../utils/AppError')
 
 
 describe("UserCreateService", () => {
+    let userRepositoryInMemory = null
+    let userCreateService = null
+
+    beforeEach(() => {
+     userRepositoryInMemory = new UserRepositoryInMemory()
+     userCreateService = new UserCreateService(userRepositoryInMemory)
+    })
+
   it("user should be create", async () => {
     const user = {
       name: "User test",
       email: "user@test.com",
       password: "123"
     }
-    
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
+
     const userCreated = await userCreateService.execute(user)
   
     expect(userCreated).toHaveProperty('id')
   })
 
-  it("user not should be created with exist email ", async() => {
+  it("user not should be created with exist email ", async () => {
     const user1 = {
       name: 'user test 1',
       email: 'user@test.com',
       password: '123'
-    }
+    };
     
     const user2 = {
       name: 'user test 2',
       email: 'user@test.com',
       password: '456'
-    }
+    };
     
-    const userRepository = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepository)
-
     await userCreateService.execute(user1)
     await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError('Este e-mail já está em uso.'))
   })
